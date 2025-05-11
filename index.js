5b6f956246806b94405b08749e6ff233a68e1eb8
AriHyuk <ariawl0209@gmail.com>
2025-05-11 23:00:45 +0700

    UPDATE : set up expess environment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("Hello, world!");
+const express = require('express');
+const app = express();
+const bookRoutes = require('./routes/bookRoutes');
+
+app.use(express.json());
+app.use('/api/books', bookRoutes);
+
+app.listen(3000, () => {
+console.log('🚀 Server running at http://localhost:3000');
+});
