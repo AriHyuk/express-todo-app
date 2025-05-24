731d04c8f64fb13b25a510efcf1062d684a8acf1
AriHyuk <coygalang990@gmail.com>
2025-05-24 21:08:50 +0700

    add index.html

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const todoRoutes = require("./routes/todos");
+const cors = require("cors");
 const PORT = 3000;
 
+app.use(cors)
 app.use(express.json());
 app.use("/api/todos", todoRoutes);
 
